bf6b3f535d3fa01f90a9a93bbeaa96a5dede4817
mza <mzandrew@gmail.com>
2023-05-09 18:55:53 -1000

    reworked the script so that multiple sets (kanji-base, kanji-compound, verbs, vocab, etc) of latex can be output by the script in quick succession; sort output files into work/ pdf/ and anki/; associated makefile changes

--- latex/日本語.py
+++ latex/日本語.py
@@ -2,13 +2,13 @@
 
 # written 2023-04-14 by mza
 # based on https://github.com/kerrickstaley/genanki
-# last updated 2023-04-30 by mza
+# last updated 2023-05-09 by mza
 
 # "I'm-learning-Japanese-I-think-I'm-learning-Japanese-I-really-think-so"
 
-FONT_SIZE = "12pt" # allowed values in extarticle are 8pt, 9pt, 10pt, 11pt, 12pt, 14pt, 17pt and 20pt
-
 import re
+from random import shuffle
+import genanki
 
 entries = []
 duplicate_entries = []
@@ -97,15 +97,17 @@ def sort_by(order):
 	global entries
 	if "hiragana-alphabetical"==order:
 		entries = sorted(entries)
-		for entry in entries:
-			print(str(entry[0]))
+#		for entry in entries:
+#			print(str(entry[0]))
 	elif "lesson"==order:
 		entries = sorted(entries, key=lambda x: x[4])
-		for entry in entries:
-			print(str(entry[4]) + " " + str(entry[0]))
-	else:
-		for entry in entries:
-			print(str(entry[0]))
+#		for entry in entries:
+#			print(str(entry[4]) + " " + str(entry[0]))
+	elif "shuffle"==order:
+		shuffle(entries)
+#	else:
+#		for entry in entries:
+#			print(str(entry[0]))
 
 def deduplicate():
 	global entries
@@ -123,7 +125,16 @@ def deduplicate():
 	#entries = list(uniq)
 	entries = new_entries
 
-anki_style = """
+def summary():
+	if len(duplicate_entries):
+		print("found " + str(len(duplicate_entries)) + " duplicate entries:")
+		for entry in duplicate_entries:
+			print(str(entry))
+	else:
+		print("no duplicate entries!")
+
+def anki_style():
+	return """
 .card {
 	font-size: 48px;
 	text-align: center;
@@ -132,8 +143,13 @@ anki_style = """
 }
 """
 
-def do_anki():
-	import genanki
+def setup_anki_decks():
+	global vocab_3way_deck
+	global vocab_3way_model
+	global kanji_base_deck
+	global kanji_base_model
+	global vocab_deck
+	global vocab_model
 	vocab_3way_deck = genanki.Deck(2342356, 'vocab 3-way')
 	vocab_3way_fields = [
 		{'name': 'kanji'},
@@ -145,9 +161,7 @@ def do_anki():
 		{ 'name': 'hiragana', 'qfmt': '{{hiragana}}', 'afmt': '{{FrontSide}}<hr id="answer">{{kanji}}   <hr>{{English}}' },
 		{ 'name': 'English',  'qfmt': '{{English}}',  'afmt': '{{FrontSide}}<hr id="answer">{{kanji}}   <hr>{{hiragana}}' }
 	]
-	vocab_3way_model = genanki.Model(293487, '3-way vocab', fields=vocab_3way_fields, templates=vocab_3way_cards, css=anki_style)
-	vocab_3way_count = 0
-	vocab_3way_entries = []
+	vocab_3way_model = genanki.Model(293487, '3-way vocab', fields=vocab_3way_fields, templates=vocab_3way_cards, css=anki_style())
 	#print(str(vocab_3way_model))
 	kanji_base_deck = genanki.Deck(6463537, 'kanji base')
 	kanji_base_fields = [
@@ -160,9 +174,7 @@ def do_anki():
 		#{ 'name': 'hiragana', 'qfmt': '{{hiragana}}', 'afmt': '{{FrontSide}}<hr id="answer">{{kanji}}   <hr>{{English}}' },
 		{ 'name': 'English',  'qfmt': '{{English}}',  'afmt': '{{FrontSide}}<hr id="answer">{{kanji}}   <hr>{{hiragana}}' }
 	]
-	kanji_base_model = genanki.Model(92929237, 'kanji base', fields=kanji_base_fields, templates=kanji_base_cards, css=anki_style)
-	kanji_base_count = 0
-	kanji_base_entries = []
+	kanji_base_model = genanki.Model(92929237, 'kanji base', fields=kanji_base_fields, templates=kanji_base_cards, css=anki_style())
 	#print(str(kanji_base_model))
 	vocab_deck = genanki.Deck(3563948, 'vocab')
 	vocab_fields = [
@@ -173,9 +185,101 @@ def do_anki():
 		{ 'name': 'hiragana', 'qfmt': '{{hiragana}}', 'afmt': '{{FrontSide}}<hr id="answer">{{English}}' },
 		{ 'name': 'English',  'qfmt': '{{English}}',  'afmt': '{{FrontSide}}<hr id="answer">{{hiragana}}' }
 	]
-	vocab_model = genanki.Model(487293, 'vocab', fields=vocab_fields, templates=vocab_cards, css=anki_style)
+	vocab_model = genanki.Model(487293, 'vocab', fields=vocab_fields, templates=vocab_cards, css=anki_style())
+
+def latex_header(font_size):
+	return r"""\documentclass[""" + font_size + r""",twocolumn]{extarticle}
+\usepackage{CJKutf8}
+\usepackage[overlap,CJK]{ruby}
+\usepackage[table]{xcolor}
+\renewcommand{\rubysep}{-0.2ex}
+\renewcommand{\rubysize}{0.7}
+\pagenumbering{gobble}
+\usepackage[portrait,left=12mm,right=8mm,top=3mm,bottom=3mm]{geometry}
+\definecolor{verylightgray}{rgb}{0.9, 0.9, 0.9}
+\begin{document}
+\begin{CJK}{UTF8}{min}
+\rowcolors{1}{white}{verylightgray}
+%\begin{tabular}{l|l}
+
+"""
+
+def latex_footer():
+	return r"""
+%\end{tabular}
+\end{CJK}
+\end{document}
+
+"""
+
+def latex_start_tabular():
+	return "\n" + r"""\begin{tabular}{l|l}"""
+
+def latex_end_tabular():
+	return r"""\end{tabular}""" + "\n"
+
+def tabular_break():
+	return latex_end_tabular() + "\n" + latex_start_tabular() + "\n"
+
+def show_hiragana_alphabetical_order():
+	return "\nあかさたなはまやらわん\n"
+
+def write_latex_file(filename, number_of_lines_per_tabular, font_size):
+	latex_entries = []
+	for hiragana, english, kanji, kanji_furigana, lesson, part_of_speech in entries:
+		japanese = hiragana
+		if not ""==kanji:
+			japanese = kanji
+		if not ""==kanji_furigana and not "\\ruby{}{}"==kanji_furigana:
+			japanese = kanji_furigana
+		my_fields = [ hiragana, japanese, english, lesson, part_of_speech ]
+		#if not "kanji-base"==part_of_speech:
+		latex_entries.append(my_fields)
+	latex_count = len(latex_entries)
+	print("found " + str(latex_count) + " latex entries")
+	line_count_so_far_for_this_tabular = 0
+	with open(filename, "w") as my_file:
+		my_file.write(latex_header(font_size))
+		#my_file.write(show_hiragana_alphabetical_order())
+		my_file.write(latex_start_tabular())
+		for entry in latex_entries:
+			line_count_so_far_for_this_tabular += 1
+			#print(entry)
+			#print(str(entry[1]) + " " + str(entry[2]))
+			my_file.write("\ " + str(entry[1]) + "&\small{" + str(entry[2]) + "}\\\\\n")
+			if number_of_lines_per_tabular<=line_count_so_far_for_this_tabular:
+				my_file.write(tabular_break())
+				line_count_so_far_for_this_tabular = 0
+		my_file.write(latex_end_tabular())
+		my_file.write(latex_footer())
+
+def do_latex():
+	# allowed values of font_size in extarticle are 8pt, 9pt, 10pt, 11pt, 12pt, 14pt, 17pt and 20pt
+	# order can be: "natural", "hiragana-alphabetical", "lesson" and "shuffle"
+	lessons = [ 0, 1, 2, 3, 4, 5, 6, 7, 8 ]
+	parse_csv_file(); print("kanji-base"); filter_include_parts_of_speech(["kanji-base"]); sort_by("natural"); write_latex_file("日本語.kanji-base.tex", 32, "12pt")
+	parse_csv_file(); print("kanji-compound"); filter_include_parts_of_speech(["kanji-compound"]); sort_by("natural"); write_latex_file("日本語.kanji-compound.tex", 32, "12pt")
+	parse_csv_file(); print("vocab"); filter_exclude_parts_of_speech(["kanji-base", "expression", "title", "name", "place", "field_of_study"]); sort_by("hiragana-alphabetical"); write_latex_file("日本語.vocab.tex", 39, "12pt")
+	parse_csv_file(); print("verbs"); filter_include_parts_of_speech(["verb"]); sort_by("hiragana-alphabetical"); write_latex_file("日本語.verbs.tex", 39, "12pt")
+	if do_lesson_by_lesson:
+		for lesson in lessons:
+			lesson_string = "lesson" + str(lesson)
+			parse_csv_file(); filter_include_lessons([lesson_string]); filter_include_parts_of_speech(["kanji-base"]); sort_by("natural"); write_latex_file("日本語." + lesson_string + ".kanji-base.tex", 32, "12pt")
+			parse_csv_file(); filter_include_lessons([lesson_string]); filter_include_parts_of_speech(["kanji-compound"]); sort_by("natural"); write_latex_file("日本語." + lesson_string + ".kanji-compound.tex", 32, "12pt")
+			parse_csv_file(); filter_include_lessons([lesson_string]); filter_exclude_parts_of_speech(["kanji-base", "expression", "title", "name", "place", "field_of_study"]); sort_by("hiragana-alphabetical"); write_latex_file("日本語." + lesson_string + ".vocab.tex", 39, "12pt")
+
+def do_anki(anki_output_file):
+	global vocab_3way_count
+	global kanji_base_count
+	global vocab_count
+	vocab_3way_count = 0
+	kanji_base_count = 0
 	vocab_count = 0
+	vocab_3way_entries = []
+	kanji_base_entries = []
 	vocab_entries = []
+	setup_anki_decks()
+	parse_csv_file()
 	for hiragana, english, kanji, kanji_furigana, lesson, part_of_speech in entries:
 		my_fields = [ hiragana, english ]
 		#print(str(my_fields))
@@ -212,111 +316,14 @@ def do_anki():
 		decks.append(vocab_deck)
 	genanki.Package(decks).write_to_file(anki_output_file)
 
-latex_header = r"""\documentclass[""" + FONT_SIZE + r""",twocolumn]{extarticle}
-\usepackage{CJKutf8}
-\usepackage[overlap,CJK]{ruby}
-\usepackage[table]{xcolor}
-\renewcommand{\rubysep}{-0.2ex}
-\renewcommand{\rubysize}{0.7}
-\pagenumbering{gobble}
-\usepackage[portrait,left=12mm,right=8mm,top=3mm,bottom=3mm]{geometry}
-\definecolor{verylightgray}{rgb}{0.9, 0.9, 0.9}
-\begin{document}
-\begin{CJK}{UTF8}{min}
-\rowcolors{1}{white}{verylightgray}
-%\begin{tabular}{l|l}
-
-"""
-latex_footer = r"""
-%\end{tabular}
-\end{CJK}
-\end{document}
-
-"""
-
-def latex_start_tabular():
-	return "\n" + r"""\begin{tabular}{l|l}"""
-
-def latex_end_tabular():
-	return r"""\end{tabular}""" + "\n"
-
-def tabular_break():
-	return latex_end_tabular() + "\n" + latex_start_tabular() + "\n"
-
-def show_hiragana_alphabetical_order():
-	return "\nあかさたなはまやらわん\n"
-
-def do_latex():
-	latex_entries = []
-	for hiragana, english, kanji, kanji_furigana, lesson, part_of_speech in entries:
-		japanese = hiragana
-		if not ""==kanji:
-			japanese = kanji
-		if not ""==kanji_furigana and not "\\ruby{}{}"==kanji_furigana:
-			japanese = kanji_furigana
-		my_fields = [ hiragana, japanese, english, lesson, part_of_speech ]
-		#if not "kanji-base"==part_of_speech:
-		latex_entries.append(my_fields)
-	latex_count = len(latex_entries)
-	print("found " + str(latex_count) + " latex entries")
-	line_count_so_far_for_this_tabular = 0
-	with open(latex_output_file, "w") as my_file:
-		my_file.write(latex_header)
-		#my_file.write(show_hiragana_alphabetical_order())
-		my_file.write(latex_start_tabular())
-		for entry in latex_entries:
-			line_count_so_far_for_this_tabular += 1
-			#print(entry)
-			#print(str(entry[1]) + " " + str(entry[2]))
-			my_file.write("\ " + str(entry[1]) + "&\small{" + str(entry[2]) + "}\\\\\n")
-			if NUMBER_OF_LINES_PER_TABULAR<=line_count_so_far_for_this_tabular:
-				my_file.write(tabular_break())
-				line_count_so_far_for_this_tabular = 0
-		my_file.write(latex_end_tabular())
-		my_file.write(latex_footer)
-
-def summary():
-	if len(duplicate_entries):
-		print("found " + str(len(duplicate_entries)) + " duplicate entries:")
-		for entry in duplicate_entries:
-			print(str(entry))
-	else:
-		print("no duplicate entries!")
-
-# usage:
-# ./日本語.py && pdflatex 日本語.tex
-
-# ---------- user settings ----------
-
 modes = [ "anki", "latex" ]
-
-anki_output_file = "日本語.apkg"
-latex_output_file = "日本語.tex"
-
-order = "natural"
-#order = "hiragana-alphabetical"
-#order = "lesson"
-
-NUMBER_OF_LINES_PER_TABULAR = 32
-#NUMBER_OF_LINES_PER_TABULAR = 36
-#NUMBER_OF_LINES_PER_TABULAR = 43
-
-# -----------------------------------
-
-parse_csv_file()
-#deduplicate()
-#filter_include_lessons(["lesson8.1", "lesson8.3"])
-#filter_include_lessons(["lesson7", "lesson8"])
-#filter_include_lessons(["lesson0", "lesson1"])
-#filter_include_lessons(["lesson7"])
-filter_include_parts_of_speech(["kanji-base"])
-#filter_include_parts_of_speech(["verb"])
-#filter_exclude_parts_of_speech(["kanji-base", "expression", "title", "name", "place", "field_of_study"])
-sort_by(order)
+do_lesson_by_lesson = False
 for mode in modes:
 	if "anki"==mode:
-		do_anki()
+		do_anki("日本語.apkg")
 	if "latex"==mode:
 		do_latex()
+
+#deduplicate()
 #summary()
 
